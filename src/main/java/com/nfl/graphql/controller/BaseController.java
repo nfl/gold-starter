dd830d5b449a7ae37fe5203de0efce39bcd050d3
earl.nolan <earl.nolan@nfl.com>
2017-12-11 17:58:18 -0500

    Initial controllers, have a wiring issue with GOLD that needs to be solved.

---
+++ src/main/java/com/nfl/graphql/controller/BaseController.java
@@ -0,0 +1,38 @@
+package com.nfl.graphql.controller;
+
+import com.nfl.dm.shield.dynamic.domain.BaseKey;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+
+import java.util.*;
+
+public class BaseController {
+
+    static final String SCHEMA_NAMESPACE = "GOLD_STARTER_SCHEMA_NAMESPACE";
+    static final String INSTANCE_NAMESPACE = "GOLD_STARTER_INSTANDE_NAMESPACE";
+
+
+    ResponseEntity<Object> buildSuccessResponse(Object data) {
+        LinkedMultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        String ttl = "30";      // seconds
+        List<String> cacheValues = new LinkedList<>();
+        cacheValues.add("max-age=" + ttl);
+        cacheValues.add("s-maxage=" + ttl);
+
+        headers.put(HttpHeaders.CACHE_CONTROL, cacheValues);
+        headers.put(HttpHeaders.CONTENT_TYPE, Collections.singletonList(MediaType.APPLICATION_JSON.toString()));
+        return new ResponseEntity<>(data, headers, HttpStatus.OK);
+    }
+
+    Map<String, Object> buildVariablesMap(String dynamicDomainName) {
+        Map<String, Object> retMap = new HashMap<>(89);
+        retMap.put(BaseKey.DYNAMIC_TYPE_NAMESPACE, SCHEMA_NAMESPACE);
+        retMap.put(BaseKey.DYNAMIC_INSTANCE_NAMESPACE, INSTANCE_NAMESPACE);
+        retMap.put(BaseKey.DYNAMIC_TYPE_NAME, dynamicDomainName);
+        return retMap;
+    }
+
+}
---
+++ src/main/java/com/nfl/graphql/controller/SchemaDefinitionController.java
@@ -0,0 +1,43 @@
+package com.nfl.graphql.controller;
+
+import com.nfl.dm.shield.dynamic.security.SchemaWriteAccess;
+import com.nfl.dm.shield.dynamic.service.GraphQLResult;
+import com.nfl.dm.shield.dynamic.service.GraphQLSchemaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@SuppressWarnings("unused")
+@Controller
+public class SchemaDefinitionController extends BaseController {
+
+    private final GraphQLSchemaService graphQLSchemaService;
+
+    @Autowired
+    public SchemaDefinitionController(GraphQLSchemaService graphQLSchemaService) {
+        this.graphQLSchemaService = graphQLSchemaService;
+    }
+
+    @RequestMapping(value = "/fe-store/definitions", method = {RequestMethod.GET, RequestMethod.POST}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> definitionsController(@RequestParam("query") String query) {
+
+        SchemaWriteAccess mutablePerms = new SchemaWriteAccess();
+        mutablePerms.addPermission(SCHEMA_NAMESPACE, SchemaWriteAccess.SCHEMA_MODIFY);
+
+        GraphQLResult result = graphQLSchemaService.executeQuery(query, buildVariablesMap(""), mutablePerms);
+
+        // add a data container for the graphiql introspection query
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("data", result.getData());
+        return result.isSuccessful() ? buildSuccessResponse(response) :
+                new ResponseEntity<>(result.getErrors().get(0), HttpStatus.BAD_REQUEST);
+    }
+}
